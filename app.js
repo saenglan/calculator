9e90f72fbfede63178be5d7b353b6597f783a8aa
Sam England <samuel.england1@gmail.com>
2021-11-22 11:54:05 -0500

    Setup express server

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
 const app = express();
+
 const port = 3000;
+
+app.get("/", function (request, response) {
+  console.log(__dirname + "./html/index.html");
+  response.sendFile(__dirname + "/public/html/index.html");
+});
 
 app.listen(port, function () {
   console.log(`Server started on port ${port}`);
