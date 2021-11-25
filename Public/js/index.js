201ed687fe10ab2b4f6b1de9c3effbbeb618c478
Sam England <samuel.england1@gmail.com>
2021-11-24 19:55:38 -0500

    Setup number variables

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,9 +1,10 @@
-let num1;
-let operator;
-let num2;
+let num1 = "";
+let operator = "";
+let num2 = "";
 
 document.addEventListener("click", function (event) {
   const id = event.target.id;
   const btn = document.getElementById(id).innerText;
-  console.log(btn);
+  num1 = num1 + btn;
+  console.log(num1);
 });
