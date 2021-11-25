53b90a7c8fb7ce8a5cbb0dabc901eeeb2e1427fa
Sam England <samuel.england1@gmail.com>
2021-11-24 19:37:41 -0500

    Draft frontend logic

---
+++ Public/js/index.js
@@ -0,0 +1,9 @@
+let num1;
+let operator;
+let num2;
+
+document.addEventListener("click", function (event) {
+  const id = event.target.id;
+  const btn = document.getElementById(id).innerText;
+  console.log(btn);
+});
