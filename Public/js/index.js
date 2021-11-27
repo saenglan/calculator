1aa9c177e901f4bf3a30858e84865236f5a1b53c
Sam England <samuel.england1@gmail.com>
2021-11-27 06:59:24 -0500

    Create conditions for buttons

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,35 +1,56 @@
 let num1 = "";
-let operator = "";
-let num2 = "";
+let operator = " ";
+let num2 = " ";
+let isFirstNum = true;
+
+let numIds = [];
+let opIds = [];
+let specIds = [];
 const display = document.getElementById("30a4d507-0a1d-4029-afdb-4bdf0acd0bdb");
 
-const numArray = document.querySelectorAll(".calc-number");
+const numArray = Array.from(document.querySelectorAll(".calc-number"));
 
 for (let i = 0; i < numArray.length; i++) {
-  document.addEventListener("click", function (event) {
-    const id = event.target.id;
-    const btn = document.getElementById(id);
-    num1 = num1 + btn.value;
-  });
+  numIds.push(numArray[i].attributes.id.value);
 }
-console.log(num1);
-// document.addEventListener("click", function (event) {
-//   const id = event.target.id;
-//   const btn = document.getElementById(id);
-//   //To Do:
-//   if (
-//     btn.classList.contains("calc-history") !== true ||
-//     btn.classList.contains("calc-display") !== true
-//   ) {
-//     console.log(btn.classList.contains("calc-history"));
-//     if (btn.value === "CLR") {
-//       num1 = "";
-//     } else {
-//       num1 = num1 + btn.value;
-//     }
-//     display.value = num1;
-//     console.log(Number(display.value));
-//   } else {
-//     console.log("Input or History Clicked");
-//   }
-// });
+const opArray = Array.from(document.querySelectorAll(".calc-operator"));
+
+for (let i = 0; i < opArray.length; i++) {
+  opIds.push(opArray[i].attributes.id.value);
+}
+const specArray = Array.from(document.querySelectorAll(".calc-special"));
+
+for (let i = 0; i < specArray.length; i++) {
+  specIds.push(specArray[i].attributes.id.value);
+}
+
+document.addEventListener("click", function (event) {
+  const id = event.target.id;
+  const btn = document.getElementById(id);
+  if (numIds.includes(id)) {
+    if (isFirstNum === true) {
+      num1 = num1 + btn.value;
+    } else {
+      num2 = num2 + btn.value;
+    }
+  } else if (opIds.includes(id)) {
+    if (isFirstNum === true) {
+      operator = btn.value;
+      isFirstNum = false;
+    } else if (isFirstNum === false) {
+      num1 = "0";
+      operator = "";
+      num2 = "0";
+      isFirstNum = true;
+    }
+  } else if (specIds.includes(id)) {
+  } else {
+  }
+  document;
+
+  console.log(Number(num1));
+  console.log(operator);
+  console.log(Number(num2));
+});
+
+document.getElementById(display).value = num1 + operator + num2;
