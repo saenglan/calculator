35c6f2865fb23b9578b98cbad868fe7bb0a8b5c6
Sam England <samuel.england1@gmail.com>
2021-11-28 06:32:06 -0500

    Add clear calculator and clear history functions

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,19 +1,35 @@
 let total = "";
+isDisplayClear = true;
+clearCount = 0;
 
 //Get display element
 const displayId = document.querySelector(".calc-display").id;
 //Get display value
 let display = document.getElementById(displayId);
 
+//Get history element
+const historyId = document.querySelector(".calc-history").id;
+//Get history value
+let history = document.getElementById(historyId);
+
+//Get clear element
+const clearId = document.querySelector(".calc-clear").id;
+//Clear calculator
+let clear = document
+  .getElementById(clearId)
+  .addEventListener("click", checkClicks);
+
 //Get submit element
 const submitId = document.querySelector(".calc-submit").id;
-//Evaluate string with Function()
+//Evaluate submitted string with Function()
 let submit = document
   .getElementById(submitId)
   .addEventListener("click", function () {
     let result = Function("return " + total)();
     display.value = result;
+    history.value = total + " = " + result;
     total = result;
+    isDisplayClear = false; //Prevents users from accidentally appending numbers to their displayed result
   });
 
 //Get array of number butons
@@ -23,7 +39,15 @@ for (let i = 0; i < numArray.length; i++) {
   numArray[i].addEventListener("click", function (event) {
     const id = event.target.id;
     const btn = document.getElementById(id);
-    addToNumber(btn.value);
+    //Clears the display if the user presses a number while the result is displayed
+    if (isDisplayClear) {
+      addToNumber(btn.value);
+    } else {
+      console.log(isDisplayClear);
+      total = "";
+      addToNumber(btn.value);
+      isDisplayClear = true;
+    }
   });
 }
 
@@ -34,7 +58,13 @@ for (let i = 0; i < oprArray.length; i++) {
   oprArray[i].addEventListener("click", function (event) {
     const id = event.target.id;
     const btn = document.getElementById(id);
-    addToNumber(btn.value);
+    //Allows user to use what is currently on the display in their next query
+    if (isDisplayClear) {
+      addToNumber(btn.value);
+    } else {
+      isDisplayClear = true;
+      addToNumber(btn.value);
+    }
   });
 }
 
@@ -43,4 +73,32 @@ function addToNumber(btn) {
   total = total + btn;
   display.value = total;
   console.log(total);
+}
+function checkClicks() {
+  // After each click, the function checks if the click was a single or double after a delay of 300ms.
+  // This gives the user 300ms to click the button again. If clicked again, the first click will result in a "single-click",
+  // but the second click will result in a "double-click" because 300ms was enough time to update clearCount to 2
+  clearCount++;
+  console.log(clearCount);
+  if (clearCount === 1) {
+    setTimeout(function () {
+      if (clearCount === 1) {
+        clearCalculator();
+      } else {
+        clearHistory();
+      }
+      clearCount = 0;
+    }, 300);
+  }
+}
+function clearCalculator() {
+  total = "";
+  display.value = total;
+  isDisplayClear = true;
+}
+function clearHistory() {
+  total = "";
+  display.value = total;
+  history.value = total;
+  isDisplayClear = true;
 }
